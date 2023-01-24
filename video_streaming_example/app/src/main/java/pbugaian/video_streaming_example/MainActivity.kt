bf0a1240fca2910e3ece86bc66a3ff2f6f785974
pbugaian <shebster603@gmail.com>
2023-01-24 23:31:02 +0200

    Added basic Android MVVM app

---
+++ video_streaming_example/app/src/main/java/pbugaian/video_streaming_example/MainActivity.kt
@@ -0,0 +1,18 @@
+package pbugaian.video_streaming_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pbugaian.video_streaming_example.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
